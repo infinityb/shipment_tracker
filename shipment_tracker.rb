772041239bfef2fb39ace8fb9f7ac2190982bfa7
Stacey Ell <stacey.ell@gmail.com>
2011-05-08 15:00:53 -0600

    Initial

---
+++ shipment_tracker.rb
@@ -0,0 +1,127 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+class ShipmentTrackerPlugin < Plugin
+	class ShipmentStatusRecord < Struct.new(:label, :status)
+		def to_s
+			if self.status
+				"\00303#{label}\003: #{status.ircify}"
+			else
+				"\00303#{label}\003: No information available."
+			end
+		end
+	end
+
+	def initialize
+		super
+		@tracking_numbers = {
+			#'Case' => '1ZX799390310081385',
+			#'Everything else' => '1ZX799331231131463'
+			#'Disks' => {:number => '1Z462E560321092067', :courier => 'ups'},
+			#'Fans' => {:number => '1ZX799470329512449', :courier => 'ups'},
+			#'Monitor' => {:number => '134619891746049', :courier => 'fedex'}
+			# "Final Fantasy™ XIV COLLECTOR'S EDITION" => {:number => '1Z04462W1300903990', :courier => 'ups'}
+			#"IB's dicks" => {:number => '1Z8FX6286801195292', :courier => 'ups'},
+			#"WoW Anthology" => {:number => '1ZA7810W0398708948', :courier => 'ups'}
+			#"Overpriced HID" => {:number => '1Z4F37F10399609311', :courier => 'ups'}
+			#"魔法少女リリカルなのは　The MOVIE 1st＜初回限定版＞" => {:number => '424981299085', :courier => 'fedex'}
+			#'Disk' => {:number => '1ZX799470342426740', :courier => 'ups'},
+			#'WiMAX CPE' => {:number => '485264002054', :courier => 'fedex'},
+			#'WiMAX CPE the 2nd' => {:number => '485264110765', :courier => 'fedex'},
+			#'WiMAX CPE - ODU ' => {:number => '1Z3X3F271343232801', :courier => 'ups'},
+			#'Plus Headphones' => {:number => '1Z0X118A1210790602', :courier => 'ups'},
+			#'HD 280 Pro' => {:number => '1Z5993920144768026', :courier => 'ups'},
+			#'Gentech CPE' => {:number => '1Z07R37W9096472131', :courier => :ups},
+			'IBの6950' => {:number => 'KCV000120856', :courier => :newegg},
+		}
+	end # initialize 
+
+	private
+	def get_scraper_manager
+		@bot.plugins[:shipmenttrackingutility].scrapers
+	end
+
+	def status_fetch(label)
+		info = @tracking_numbers[label]
+		ShipmentStatusRecord.new(label, get_scraper_manager.fetch(info[:courier], info[:number]))
+	end # status_fetch
+
+	public
+	def status_all(m, params)
+		@tracking_numbers.keys.each {|label|
+			if not get_scraper_manager.has_courier?(@tracking_numbers[label][:courier])
+				m.reply "#{label}: Sorry, that courier service is not supported. :("
+			end
+			ssr =  status_fetch(label)
+			if ssr
+				m.reply ssr
+			else # status = nil
+				m.reply "#{label}: Sorry, no information is available."
+			end
+		}
+        rescue Exception => e
+                m.reply e.class
+                m.reply e
+	end # status
+
+	def status_unnamed(m, params)
+		number = params[:number]
+		courier = params[:courier].downcase
+
+		if get_scraper_manager.has_courier?(courier)
+			status = get_scraper_manager.fetch(courier, number)
+
+			if status
+				m.reply status
+			else # status = nil
+				m.reply "Sorry, no information is available."
+			end # status
+		else
+			m.reply "Sorry, that courier service is not supported. :("
+		end
+	rescue Exception => e
+		m.reply e.class
+		m.reply e
+	end # status_unnamed
+
+	def status_named(m, params)
+		label = params[:label]
+
+		if @tracking_numbers.has_key?(label)
+			if not get_scraper_manager.has_courier?(@tracking_numbers[label][:courier])
+				m.reply "Sorry, that courier service is not supported. :("
+				return
+			end
+			ssr = status_fetch(label)
+			if ssr
+				m.reply ssr
+			else # status = nil
+				m.reply "Sorry, no information is available."
+			end
+		else
+			m.reply "Sorry, I don't know a shipment by that name"
+		end
+	rescue Exception => e
+		m.reply e.class
+		m.reply e
+	end
+
+	def cron_notify(m, params)
+		status_fetch.each {|msg|
+                        @bot.say '#', msg if msg.status.ircify != @registry[msg.label]
+                        @registry[msg.label] = msg.status.ircify
+                }
+        end # cron_notify
+
+end # ShipmentTrackerPlugin
+
+plugin = ShipmentTrackerPlugin.new
+plugin.default_auth('notify', false)
+
+plugin.map 'shipment', :action => 'status_all'
+plugin.map 'shipment :label', :action => 'status_named'
+plugin.map 'shipment :number :courier', :action => 'status_unnamed'
+#plugin.map 'shipment add ":label" :number :courier', :action => 'add_shipment'
+plugin.map 'shipment cron_notify', :action => 'cron_notify', :auth_path => 'notify'
+
---
+++ shipment_tracking_utility.rb
@@ -0,0 +1,183 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'json' # fuck fedex in the ass
+
+class ShipmentTrackingUtilityPlugin < Plugin
+	class ShipmentStatus
+		@number = nil
+		@location = nil
+		@time = nil
+		@activity = nil
+		@carrier = nil
+
+		attr_reader :number, :location, :time, :activity, :carrier
+
+		def initialize(data)
+			@number = data[:number]
+			@location = data[:location]
+			@time = data[:time]
+			@activity = data[:activity]
+			@carrier = data[:carrier]
+		end # initialize
+
+		def ircify
+			"#{activity} @ #{time} - #{location}"
+		end
+	end # ShipmentStatus
+
+	module Scrapers
+		module UPS
+			NAME_KEYS = [:ups]
+			PRIMARY_NAME = 'UPS'
+
+			def self.fetch(number)
+				doc = Nokogiri::HTML(open("http://wwwapps.ups.com/WebTracking/processInputRequest?sort_by=status&tracknums_displayed=1&TypeOfInquiryNumber=T&loc=en_US&InquiryNumber1=#{number}&track.x=0&track.y=0"))
+				latest_row = doc.search('div[@id=collapse3]').first
+
+				if latest_row
+					latest_row = latest_row.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.search('tr')[1]
+
+					status = ShipmentStatus.new(
+						:number => number,
+						:location => latest_row.children[0].children.first.content.ircify_html,
+						:time => latest_row.children[2].content.ircify_html << " " << latest_row.children[4].content.ircify_html,
+						:activity => latest_row.children[6].content.ircify_html,
+						:carrier => PRIMARY_NAME
+					)
+				else
+					nil
+				end
+			end # fetch UPS
+		end
+
+		module FedEx
+			NAME_KEYS = [:fedex]
+			PRIMARY_NAME = 'FedEx'
+
+			def self.fetch(number)
+				doc = open("http://www.fedex.com/Tracking?language=english&cntry_code=us&tracknumbers=#{number}")
+
+				data = ''
+				doc.each_line do |line|
+					if line =~ /^var detailInfoObject/
+						data = line
+						break # fuck the rest of this page
+					end
+				end
+				data = data.sub(/var detailInfoObject = /, '').sub(/;\n$/, '')
+
+				if data == ''
+					return nil
+				end
+				# keys = ["scanDate", "GMTOffset", "showReturnToShipper", "scanStatus", "scanLocation", "scanTime", "scanComments"]
+				data = JSON.parse(data)
+				latest_row = data['scans'].first
+
+				if latest_row
+					status = ShipmentStatus.new(
+						:number => number,
+						:location => latest_row['scanLocation'],
+						:time => latest_row['scanDate'] << ' ' << latest_row['scanTime'],
+						:activity => latest_row['scanStatus'],
+						:carrier => PRIMARY_NAME
+					)
+				else
+					nil
+				end
+			end # fetch fedex
+
+		end
+		module Purolator
+			NAME_KEYS = [:purolator]
+			PRIMARY_NAME = 'Purolator'
+
+			def self.fetch(number)
+				doc = Nokogiri::HTML(open("https://eshiponline.purolator.com/SHIPONLINE/Public/Track/TrackingDetails.aspx?pin=#{number}"))
+				latest_row = doc.search('').first
+
+				if latest_row
+					latest_row = latest_row #.tranformations if needed
+
+					status = ShipmentStatus.new(
+						:number => number,
+						:location => :NotImplementedError,
+						:time => :NotImplementedError,
+						:activity => :NotImplementedError,
+						:carrier => PRIMARY_NAME
+					)
+				else
+					nil
+				end
+			end
+		end
+		module Newegg
+			NAME_KEYS = [:newegg]
+			PRIMARY_NAME = 'newegg'
+
+			def self.fetch(number)
+				doc = Nokogiri::HTML(open("http://www.newegg.com/Info/TrackOrder.aspx?TrackingNumber=#{number}"))
+				latest_row = doc.search('div[@id=collapse3]').first
+
+				if latest_row
+					latest_row = latest_row #.tranformations if needed
+
+					status = ShipmentStatus.new(
+						:number => number,
+						:location => :NotImplementedError,
+						:time => :NotImplementedError,
+						:activity => :NotImplementedError,
+						:carrier => PRIMARY_NAME
+					)
+				else
+					nil
+				end
+			end
+		end
+	end
+
+	class ShipmentScreenScraperManager
+		def initialize()
+			@by_name = Hash.new
+			@loaded_modules = Array.new
+		end
+
+		def register(scraper_module)
+			for name in scraper_module::NAME_KEYS
+				if @by_name.has_key?(name.to_sym)
+					raise RuntimeException, "already registered."
+				end
+				@by_name[name.to_sym] = scraper_module
+			end
+			@loaded_modules << scraper_module
+		end
+
+		def [](name)
+			@by_name[name.to_sym]
+		end
+
+		def has_courier?(name)
+			@by_name.has_key?(name.to_sym)
+		end
+
+		def fetch(courier, number)
+			self[courier.to_sym].fetch(number)
+		end
+	end
+
+	def initialize()
+		super
+		@scrapers = ShipmentScreenScraperManager.new()
+		@scrapers.register(Scrapers::UPS)
+		@scrapers.register(Scrapers::FedEx)
+		@scrapers.register(Scrapers::Purolator)
+		#@scrapers.register(Scrapers::Newegg)
+	end
+	attr_accessor :scrapers
+
+	def help(action="")
+		"This plugin is a utility plugin which is only meant to be used by other plugins."
+	end
+end
+
+ShipmentTrackingUtilityPlugin.new
